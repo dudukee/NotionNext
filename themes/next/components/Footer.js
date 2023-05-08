4c715f7edbda3615660069d597692b973eab9cc6
dudukee <dudukee@hotmail.com>
2023-05-08 12:17:15 +0800

    优化 copyrightDate 的显示

--- themes/next/components/Footer.js
+++ themes/next/components/Footer.js
@@ -18,7 +18,7 @@ const Footer = ({ title }) => {
     >
       <DarkModeButton/>
       <span>
-        <i className='fas fa-copyright' /> {`${copyrightDate}`} <span className='hidden mx-1 animate-pulse'><i className='fas fa-heart' /></span> <a href={BLOG.LINK} className='hidden underline font-bold '>{BLOG.AUTHOR}</a>.<br />
+        <i className='hidden fas fa-copyright' /> {`${copyrightDate}`} <span className='hidden mx-1 animate-pulse'><i className='fas fa-heart' /></span> <a href={BLOG.LINK} className='hidden underline font-bold '>{BLOG.AUTHOR}</a><br />
 
         {BLOG.BEI_AN && <><i className='fas fa-shield-alt' /> <a href='https://beian.miit.gov.cn/' className='mr-2'>{BLOG.BEI_AN}</a><br /></>}
 
